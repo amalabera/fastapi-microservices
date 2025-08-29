fd4ecae25d52b2d4e29295f7535b705882250614
Amala B <BERAAMALA43@GMAIL.COM>
2025-08-28 22:51:32 -0500

    Update main.py

--- fastapi-microservices/app/main.py
+++ fastapi-microservices/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.routers import users
 
-app = FastAPI(title="FastAPI Microservices Demo")
+app = FastAPI(title="FastAPI Microservices")
 
 app.include_router(users.router)
 
